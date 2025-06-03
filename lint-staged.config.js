aa1c844e78c062cfedc1fd55526983b268798152
Weston Ruter <weston@ruter.net>
2025-06-03 07:32:07 -0700

    Improve linting config

---
+++ lint-staged.config.js
@@ -0,0 +1,9 @@
+/**
+ * @type {import('lint-staged').Configuration}
+ */
+const config = {
+	'*.{js,ts,mjs}': ['npx wp-scripts lint-js', () => 'npx tsc'],
+	'*.php': ['composer phpcs', () => 'composer phpstan'],
+};
+
+module.exports = config;
--- lint-staged.config.mjs
+++
@@ -1,7 +0,0 @@
-/**
- * @type {import('lint-staged').Configuration}
- */
-export default {
-	'*.{js,ts,mjs}': [ 'npx wp-scripts lint-js', () => 'npx tsc' ],
-	'*.php': [ 'composer phpcs', () => 'composer phpstan' ],
-};
