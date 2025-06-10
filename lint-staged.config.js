4116a5615c7830290805f4fca62628b40cadd6dc
Weston Ruter <weston@ruter.net>
2025-06-10 07:11:23 -0700

    Add composer validation to lint-staged

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -3,7 +3,10 @@
  */
 const config = {
 	'*.{js,ts,mjs}': [ 'npx wp-scripts lint-js', () => 'npx tsc' ],
-	'composer.{json,lock}': () => 'composer normalize --dry-run',
+	'composer.{json,lock}': [
+		() => 'composer validate --strict',
+		() => 'composer normalize --dry-run',
+	],
 	'*.php': [ 'composer phpcs', () => 'composer phpstan' ],
 };
 
