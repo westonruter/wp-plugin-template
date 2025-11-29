06060162c22a81a41abb65e08ebea897f45989bd
Weston Ruter <weston@ruter.net>
2025-11-28 19:05:31 -0800

    Add version from package.json to version consistency checking

--- tools/verify-version-consistency.php
+++ tools/verify-version-consistency.php
@@ -21,6 +21,15 @@ if ( 'cli' !== php_sapi_name() ) {
 
 $versions = array();
 
+$package_json = json_decode( (string) file_get_contents( __DIR__ . '/../package.json' ), true );
+if ( ! is_array( $package_json ) ) {
+	echo "Failed to parse package.json\n";
+	exit( 1 );
+}
+if ( array_key_exists( 'version', $package_json ) ) {
+	$versions['package_json'] = $package_json['version'];
+}
+
 $readme_md = (string) file_get_contents( __DIR__ . '/../README.md' );
 if ( ! preg_match( '/\*\*Stable tag:\*\*\s+(?P<version>\S+)/i', $readme_md, $matches ) ) {
 	echo "Could not find stable tag in readme.\n";
